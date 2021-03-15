59a603216093191dcd2d87bd69d8c13325d6321f
youssefjk <80720800+youssefjk@users.noreply.github.com>
2021-03-15 14:24:37 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-eu.rplant.xyz:7042 -u sugar1qfkfqw6a803fnsme874m6c87pn34za9mlax6qx5.test
 
 
 done
